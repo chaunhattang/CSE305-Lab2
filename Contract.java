c074cbe9e7dd1e5eea75a9b76f21176aafc9b2f8
chaunhattang <tang.chaunhat.cit22@eiu.edu.vn>
2025-05-24 09:07:30 +0700

    Upload File for Lab 5 - Week 5
    This is all class in Problem 1 of Lab 5 - Week 5

---
+++ Contract.java
@@ -0,0 +1,11 @@
+public interface Contract {
+    void buildContractID(String contractID);
+
+    void buildPropertyID(String propertyID);
+
+    void buildTenantID(String tenantID);
+
+    void buildRentAmount(double rentAmount);
+
+    Contract signContract();
+}
